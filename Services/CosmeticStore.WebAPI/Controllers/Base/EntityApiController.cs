318a4f9c2d8b9031f800f7e94956ecc1a9eeea95
Infarh <shmachilin@yandex.ru>
2022-04-23 11:44:40 +0300

    Выделение базового класса api-контроллера

---
+++ Services/CosmeticStore.WebAPI/Controllers/Base/EntityApiController.cs
@@ -0,0 +1,73 @@
+﻿using CosmeticStore.Interfaces.Entities;
+using CosmeticStore.Interfaces.Repositories;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CosmeticStore.WebAPI.Controllers.Base;
+
+[ApiController]
+public abstract class EntityApiController<T> : ControllerBase where T : class, IEntity
+{
+    private readonly IRepository<T> _Items;
+
+    protected EntityApiController(IRepository<T> Products) => _Items = Products;
+
+    [HttpGet("count")]
+    public async Task<IActionResult> Count()
+    {
+        var count = await _Items.Count();
+        return Ok(count);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var items = await _Items.GetAllAsync();
+        if (!items.Any())
+            return NoContent();
+        return Ok(items);
+    }
+
+    [HttpGet("({Skip}:{Take})")]
+    public async Task<IActionResult> Get(int Skip, int Take)
+    {
+        var items = await _Items.GetAsync(Skip, Take);
+        if (!items.Any())
+            return NoContent();
+        return Ok(items);
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetById(int Id)
+    {
+        var item = await _Items.GetByIdAsync(Id);
+        if (item is null)
+            return NotFound();
+        return Ok(item);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] T item)
+    {
+        var id = await _Items.AddAsync(item);
+        return CreatedAtAction(nameof(GetById), new { Id = id }, item);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] T item)
+    {
+        var result = await _Items.UpdateAsync(item);
+        if (result)
+            return Ok(true);
+        return NotFound(false);
+    }
+
+    [HttpDelete("{Id}")]
+    public async Task<IActionResult> Delete(int Id)
+    {
+        var item = await _Items.RemoveAsync(Id);
+        if (item is null)
+            return NotFound();
+        return Ok(item);
+    }
+}
--- Services/CosmeticStore.WebAPI/Controllers/CategoriesApiController.cs
+++ Services/CosmeticStore.WebAPI/Controllers/CategoriesApiController.cs
@@ -1,74 +1,14 @@
 ﻿using CosmeticStore.Domain.Entities;
 using CosmeticStore.Interfaces;
 using CosmeticStore.Interfaces.Repositories;
+using CosmeticStore.WebAPI.Controllers.Base;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CosmeticStore.WebAPI.Controllers;
 
 [ApiController]
 [Route(WebAPIAddress.Categories)]
-public class CategoriesApiController : ControllerBase
+public class CategoriesApiController : EntityApiController<Category>
 {
-    private readonly IRepository<Category> _Categories;
-
-    public CategoriesApiController(IRepository<Category> Categories) => _Categories = Categories;
-
-    [HttpGet("count")]
-    public async Task<IActionResult> Count()
-    {
-        var count = await _Categories.Count();
-        return Ok(count);
-    }
-
-    [HttpGet]
-    public async Task<IActionResult> GetAll()
-    {
-        var items = await _Categories.GetAllAsync();
-        if (!items.Any())
-            return NoContent();
-        return Ok(items);
-    }
-
-    [HttpGet("({Skip}:{Take})")]
-    public async Task<IActionResult> Get(int Skip, int Take)
-    {
-        var items = await _Categories.GetAsync(Skip, Take);
-        if (!items.Any())
-            return NoContent();
-        return Ok(items);
-    }
-
-    [HttpGet("{Id}")]
-    public async Task<IActionResult> GetById(int Id)
-    {
-        var item = await _Categories.GetByIdAsync(Id);
-        if (item is null)
-            return NotFound();
-        return Ok(item);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Add([FromBody] Category item)
-    {
-        var id = await _Categories.AddAsync(item);
-        return CreatedAtAction(nameof(GetById), new { Id = id }, item);
-    }
-
-    [HttpPut]
-    public async Task<IActionResult> Update([FromBody] Category item)
-    {
-        var result = await _Categories.UpdateAsync(item);
-        if (result)
-            return Ok(true);
-        return NotFound(false);
-    }
-
-    [HttpDelete("{Id}")]
-    public async Task<IActionResult> Delete(int Id)
-    {
-        var item = await _Categories.RemoveAsync(Id);
-        if (item is null)
-            return NotFound();
-        return Ok(item);
-    }
+    public CategoriesApiController(IRepository<Category> Products) : base(Products) { }
 }
